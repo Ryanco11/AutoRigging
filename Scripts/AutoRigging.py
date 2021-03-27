114a88d4d95fd0ab59ea9e4e9f34bc2ca661b164
wcb <ytlzwcb@gmail.com>
2021-03-27 15:38:22 +0800

    Assign mesh to ARMATURE

--- Scripts/AutoRigging.py
+++ Scripts/AutoRigging.py
@@ -3,8 +3,19 @@ import bpy
 print('Try')
 
 bpy.ops.object.select_all(action='DESELECT')
-#bpy.ops.object.select_name(name='Cube', extend=False)
 
+
+#Select mesh firsy
 bpy.data.objects['Cube'].select_set(True)
+
+#select the ARMATURE
 bpy.data.objects['Armature1'].select_set(True)
+
+#apply ARMATURE to the mesh
+bpy.ops.object.parent_set(type='ARMATURE_AUTO')
+
+
+
+vertex_in_range = []
+
 
