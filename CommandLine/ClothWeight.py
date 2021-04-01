41cb75e69b78d32fa2d1234b1072de0e53c303d5
wcb <ytlzwcb@gmail.com>
2021-04-01 08:22:41 +0800

    resetting cloth weight

--- CommandLine/ClothWeight.py
+++ CommandLine/ClothWeight.py
@@ -24,59 +24,37 @@ def SetFullWeight(source_fbx_path, file_name):
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.object.delete(use_global=False)
 
-# import target mesh
-    source_mesh = bpy.ops.import_scene.fbx(filepath = source_fbx_path)
-
-# vaildate target fbx
-    # check mesh count
+# import source fbx
+    source_mesh = bpy.ops.import_scene.fbx( filepath = body_mesh_path )
+# delete source mesh
     bpy.ops.object.select_by_type(type='MESH')
-    print("mesh count is : " + str(len(bpy.context.selected_objects)))
-    if len(bpy.context.selected_objects) > 1:
-        print("mutliple mesh existing")
-        return
-    elif len(bpy.context.selected_objects) == 0:
-        print("no mesh found in target fbx")
-        return
+    bpy.ops.object.delete(use_global=False)
 
-    # check vertex group count
+# import target mesh
+    source_mesh = bpy.ops.import_scene.fbx(filepath=source_fbx_path)
+
+
+###set target mesh parent to source armature
+    a = bpy.data.objects['Bip001']
+    #select the only mesh in scene
+    bpy.ops.object.select_by_type(type='MESH')
     selection_names = bpy.context.selected_objects
-    print(selection_names[0].name)
-    mesh_root_name = selection_names[0].name
-    a = bpy.data.objects[mesh_root_name]
-    print("vertex group count is : " + str(len(a.vertex_groups)))
-    print("vertex group name is : " + a.vertex_groups[0].name)
-    # if len(a.vertex_groups) > 1:
-    #     print("mutliple vg existing")
-    #     return
-    if len(bpy.context.selected_objects) == 0:
-        print("no vg found in target fbx")
-        return
+    b = bpy.data.objects[selection_names[0].name]
 
-    # check fbx armature count
-    bpy.ops.object.select_all(action='DESELECT')
-    bpy.ops.object.select_by_type(type='ARMATURE')
-    print("ARMATURE count is : " + str(len(bpy.context.selected_objects)))
+    bpy.ops.object.select_all(action='DESELECT') # deselect all object
 
-    if len(bpy.context.selected_objects) > 1:
-        print("mutliple ARMATURE existing")
-        return
-    elif len(bpy.context.selected_objects) == 0:
-        print("no ARMATURE found in target fbx")
-        return
+    a.select_set(True)
+    b.select_set(True)     # select the object for the 'parenting'
 
-    print("armature_name is : " + bpy.context.selected_objects[0].name)
-    armature_name = bpy.context.selected_objects[0].name
+    bpy.context.view_layer.objects.active = a    # the active object will be the parent of all selected object
 
-# import body mesh
-    body_mesh = bpy.ops.import_scene.fbx(filepath = body_mesh_path)
-
-# deselect all object
-    bpy.ops.object.mode_set(mode='OBJECT')
-    bpy.ops.object.select_all(action='DESELECT')
+    # set b's parent to a
+    bpy.ops.object.parent_set(type='OBJECT', keep_transform=True)
 
 
     print("complete mesh validation, start setting weights")
 
 
-    #######define geomtory for each bone
+    ###chairfy which bone need to be used
+    ###define geomtory for each bone
     ###apply weight to each bone
